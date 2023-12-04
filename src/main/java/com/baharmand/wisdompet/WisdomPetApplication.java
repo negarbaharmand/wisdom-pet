71c57f52ee77a952224b31a4406cbf1c51a0171d
Negar Baharmand <negarbaharmand@outlook.com>
2023-12-04 13:59:54 +0100

    Initial commit

---
+++ src/main/java/com/baharmand/wisdompet/WisdomPetApplication.java
@@ -0,0 +1,14 @@
+package com.baharmand.wisdompet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisdomPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WisdomPetApplication.class, args);
+        System.out.println("HEllo!");
+    }
+
+}
